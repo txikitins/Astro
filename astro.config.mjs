e549d78eac04daf8b7ecf11ed5506e4ebd4a8634
txikitins <152338310+txikitins@users.noreply.github.com>
2024-01-13 09:50:00 +0000

    git changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://txikitins.github.io',
-
-});
+export default defineConfig({});
